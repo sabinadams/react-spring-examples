721885117437ee0f049a76423147d85f66c76219
Sabin <sabintheworld@gmail.com>
2020-08-13 16:15:54 -0700

    Removed some examples

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
-import AnimatedLoader from './examples/useSpring/animatedLoader'
 import FadingText from './examples/useSpring/fadingText'
-import ToggleText from './examples/useSpring/toggleText'
 import MorphingDivs from './examples/useSprings/morphingDivs'
 import RoundingSquares from './examples/useTrail/roundingSquares'
 import SquareToCircleList from './examples/useTransition/squareToCircleList'
@@ -12,9 +10,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <AnimatedLoader/> */}
         {/* <FadingText/> */}
-        {/* <ToggleText/> */}
         {/* <MorphingDivs/> */}
         {/* <RoundingSquares/> */}
         {/* <SquareToCircleList/> */}
